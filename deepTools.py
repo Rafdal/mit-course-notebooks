b70044547f7a68bd1fc3a40394dd2304c1825729
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2023-02-11 23:53:59 -0300

    updt

--- deepTools.py
+++ deepTools.py
@@ -2,8 +2,7 @@
 #   Herramientas recopiladas del curso de "Deep Learning: Mastering neural networks" del MIT (Enero 2023)
 #
 
-import numpy as np
-import seaborn as sns
+# import numpy as np
 
 import torch
 import torch.nn as nn
@@ -21,6 +20,27 @@ import time, copy
 import torchvision
 
 
+
+#           * [ SPLIT DATASET ] *
+
+''' USAR CON
+# Crear StandardScaler para la estandarización de datos
+# Más información aquí: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+# https://scikit-learn.org/stable/modules/generated/sklearn.compose.ColumnTransformer.html#sklearn.compose.ColumnTransformer
+ct = ColumnTransformer([('numerical_features', StandardScaler(), numerical_feature_columns)], remainder='passthrough')
+
+train_dataset, test_dataset, val_dataset = train_test_val_split(df, numerical_feature_columns, label_column, ct)
+
+# Y Luego:
+dataloaders = {'train': DataLoader(train_dataset, batch_size=batch_size),
+               'val': DataLoader(val_dataset, batch_size=batch_size),
+               'test': DataLoader(test_dataset, batch_size=batch_size)}
+
+dataset_sizes = {'train': len(train_dataset),
+                 'val': len(val_dataset),
+                 'test': len(test_dataset)}
+print(f'dataset_sizes = {dataset_sizes}')
+'''
 
 # feature_columns:  List(String)    - list of column names to be used as features (X)
 # label_column:     String          - column to be used as target (Y)
@@ -70,3 +90,261 @@ def train_test_val_split(df, feature_columns, label_column, ct):
     test_dataset = TensorDataset(test_x, test_y)
     val_dataset = TensorDataset(val_x, val_y)
     return train_dataset, test_dataset, val_dataset
+
+
+
+
+
+
+
+
+def train_model(model, device, dataloaders, dataset_sizes, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict()) # Los mejores pesos se deben guardar por separado
+    best_acc = 0.0
+    best_epoch = 0
+
+    # Cada ciclo tiene una fase de entrenamiento, una de validación y una de prueba
+    phases = ['train', 'val', 'test']
+    
+    # Hacer un seguimiento de la evolución de la pérdida y la precisión durante el entrenamiento
+    training_curves = {}
+    for phase in phases:
+        training_curves[phase+'_loss'] = []
+        training_curves[phase+'_acc'] = []
+    
+    for epoch in range(num_epochs):
+        print(f'\nEpoch {epoch+1}/{num_epochs}')
+        print('-' * 10)
+
+        for phase in phases:
+            if phase == 'train':
+                model.train()  # Configurar el modelo en el modo de entrenamiento
+            else:
+                model.eval()   # Configurar el modelo en el modo de evaluación
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterar con los datos
+            for inputs, labels in dataloaders[phase]:
+                # Importante: se deben aplanar todos los puntos de datos
+                # para que sean compatibles con el modelo
+                inputs = inputs.view(inputs.shape[0],-1)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # Poner a 0 los gradientes de los parámetros
+                optimizer.zero_grad()
+
+                # Método forward
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, predictions = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # Método backward y actualización de los pesos solo si está en la fase de entrenamiento
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # Estadísticas
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(predictions == labels.data)
+ 
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            training_curves[phase+'_loss'].append(epoch_loss)
+            training_curves[phase+'_acc'].append(epoch_acc.cpu())   # FIXME: Workaround
+
+            print(f'{phase:5} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # Hacer una copia profunda del modelo si se ha alcanzado la mejor precisión
+            if phase == 'val' and epoch_acc > best_acc:
+                best_epoch = epoch
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    time_elapsed = time.time() - since
+    print(f'\nTraining complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f} at epoch {best_epoch}')
+
+    # Cargar los mejores pesos del modelo
+    model.load_state_dict(best_model_wts)
+    
+    return model, training_curves
+
+
+
+# Extraído de https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+
+def train_regression_model(model, device, dataloaders, dataset_sizes, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = np.inf
+    best_epoch = 0
+
+    # Cada ciclo tiene una fase de entrenamiento, una de validación y una de prueba
+    phases = ['train', 'val', 'test']
+    
+    # Hacer un seguimiento de la evolución de la pérdida y la precisión durante el entrenamiento
+    training_curves = {}
+    for phase in phases:
+        training_curves[phase+'_loss'] = []
+    
+    for epoch in range(num_epochs):
+        print(f'\nEpoch {epoch+1}/{num_epochs}')
+        print('-' * 10)
+
+        for phase in phases:
+            if phase == 'train':
+                model.train()  # Configurar el modelo en el modo de entrenamiento
+            else:
+                model.eval()   # Configurar el modelo en el modo de evaluación
+            running_loss = 0.0
+
+            # Iterar con los datos
+            for inputs, targets in dataloaders[phase]: 
+                inputs = inputs.to(device)
+                targets = targets.to(device) #
+
+                # Poner a 0 los gradientes de los parámetros
+                optimizer.zero_grad()
+
+                # Método forward
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = torch.flatten(model(inputs))
+                    loss = criterion(outputs, targets)
+
+                    # Método backward y actualización de los pesos solo si está en la fase de entrenamiento
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # Estadísticas
+                running_loss += loss.item() * inputs.size(0)
+ 
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            training_curves[phase+'_loss'].append(epoch_loss)
+
+            print(f'{phase:5} Loss: {epoch_loss:.4f}')
+
+            # Hacer una copia profunda del modelo si se ha alcanzado la mejor precisión (en función de la validación)
+            if phase == 'val' and epoch_loss < best_loss:
+                best_epoch = epoch
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    time_elapsed = time.time() - since
+    print(f'\nTraining complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Loss: {best_loss:4f} at epoch {best_epoch}')
+
+    # Cargar los mejores pesos del modelo
+    model.load_state_dict(best_model_wts)
+    
+    return model, training_curves
+
+
+
+
+
+#       * [ METRICAS ] *
+
+def plot_training_curves(training_curves, 
+                         phases=['train', 'val', 'test'],
+                         metrics=['loss','acc']):
+    epochs = list(range(len(training_curves['train_loss'])))
+    for metric in metrics:
+        plt.figure()
+        plt.title(f'Training curves - {metric}')
+        for phase in phases:
+            key = phase+'_'+metric
+            if key in training_curves:
+                # for curve in training_curves:
+                    # print("TYPE: ", curve)
+                plt.plot(epochs, training_curves[phase+'_'+metric])
+        plt.xlabel('epoch')
+        plt.legend(labels=phases)
+
+def classify_predictions(model, device, dataloader):
+    model.eval()   # Configurar el modelo en el modo de evaluación
+    all_labels = torch.tensor([]).to(device)
+    all_scores = torch.tensor([]).to(device)
+    all_preds = torch.tensor([]).to(device)
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = torch.softmax(model(inputs),dim=1)
+        _, preds = torch.max(outputs, 1)
+        scores = outputs[:,1]
+        all_labels = torch.cat((all_labels, labels), 0)
+        all_scores = torch.cat((all_scores, scores), 0)
+        all_preds = torch.cat((all_preds, preds), 0)
+    return all_preds.detach().cpu(), all_labels.detach().cpu(), all_scores.detach().cpu()
+
+def plot_regression(model, device, dataloader):
+    preds, targets = regress_predictions(model, device, dataloader)
+    plt.figure()
+    plt.title(f'Regression results')
+    plt.scatter(targets, preds)
+    xmin = min(min(targets), min(preds))
+    xmax = max(max(targets), max(targets))
+    plt.plot([xmin, xmax], [xmin, xmax], 'black', linestyle='--', marker='')
+    plt.xlabel('target value')
+    plt.ylabel('prediced value')
+
+def regress_predictions(model, device, dataloader):
+    model.eval()   # Configurar el modelo en el modo de evaluación
+    all_targets = torch.tensor([]).to(device)
+    all_preds = torch.tensor([]).to(device)
+    for inputs, targets in dataloader:
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        preds = model(inputs)
+        all_targets = torch.cat((all_targets, targets), 0)
+        all_preds = torch.cat((all_preds, preds), 0)
+    return all_preds.detach().cpu(), all_targets.detach().cpu()
+
+def plot_metrics(model, device, dataloaders, phase='test'):
+    preds, labels, scores = classify_predictions(model, device, dataloaders[phase])
+
+    fpr, tpr, thresholds = metrics.roc_curve(labels, scores)
+    auc = metrics.roc_auc_score(labels, preds)
+    
+    disp = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=auc)
+    ind = np.argmin(np.abs(thresholds - 0.5))
+    ax = disp.plot().ax_
+    ax.scatter(fpr[ind], tpr[ind], color = 'red')
+    ax.set_title('ROC Curve (red dot at threshold = 0.5)')
+    
+    cm = metrics.confusion_matrix(labels, preds)
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_labels)
+    #disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm)
+    ax = disp.plot().ax_
+    ax.set_title('Confusion Matrix -- counts')
+
+    ncm = metrics.confusion_matrix(labels, preds, normalize='true')
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=ncm)
+    ax = disp.plot().ax_
+    ax.set_title('Confusion Matrix -- rates')
+
+    TN, FP, FN, TP = cm[0,0], cm[0,1], cm[1,0], cm[1,1] 
+    N, P = TN + FP, TP + FN 
+    ACC = (TP + TN)/(P+N)
+    TPR, FPR, FNR, TNR = TP/P, FP/N, FN/P, TN/N
+    print(f'\nAt default threshold:')
+    print(f' TN = {TN:5},  FP = {FP:5} -> N = {N:5}')
+    print(f' FN = {FN:5},  TP = {TP:5} -> P = {P:5}')
+    print(f'TNR = {TNR:5.3f}, FPR = {FPR:5.3f}')
+    print(f'FNR = {FNR:5.3f}, TPR = {TPR:5.3f}')
+    print(f'ACC = {ACC:6.3f}')
+
+    return cm, fpr, tpr, thresholds, auc
